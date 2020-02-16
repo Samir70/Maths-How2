11d38e13bd31717dc7c6efc63475a54ae9a1e33b
Samir70 <sbetmouni@talktalk.net>
2020-02-16 19:32:27 +0000

    move triangle list to seperate file

---
+++ docs/Geometry/Trigonometry/triangleQs.js
@@ -0,0 +1,13 @@
+const [ADJ, OPP, HYP] = ['adjacent', 'opposite', 'hypotenuse'];
+
+const triangles2Lable = [
+    { triPic: "triangles/AngleRAdj.png", answer: ADJ },
+    { triPic: "triangles/AngleROpp.png", answer: OPP },
+    { triPic: "triangles/AngleRHyp.png", answer: HYP },
+    { triPic: "triangles/AngleTopAdj.png", answer: ADJ },
+    { triPic: "triangles/AngleTopOpp.png", answer: OPP },
+    { triPic: "triangles/AngleTopHyp.png", answer: HYP },
+    { triPic: "triangles/Adj1.png", answer: ADJ },
+    { triPic: "triangles/Opp1.png", answer: OPP },
+    { triPic: "triangles/NoAngleHyp.png", answer: HYP }
+];

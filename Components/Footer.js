1b79a8acadb063db36b7bcb86e9efb697b8503ae
Samir70 <sbetmouni@talktalk.net>
2020-02-20 12:34:46 +0000

    add re-useable header and footer

---
+++ Components/Footer.js
@@ -0,0 +1,9 @@
+Vue.component('footer-links', {
+    template: `
+    <div>
+      <p>Navigate to <a href="https://samir70.github.io/Maths-How2/">Maths-How2 Contents page</a>
+      <br> Copyright Samir70. <a href="https://github.com/Samir70/Maths-How2">Github</a>
+      <br> I make these pages in my spare time. If you like them, you can buy me a coffee (once the link is up and running)</p>
+    <div>
+    `
+});
---
+++ Components/Header.js
@@ -0,0 +1,5 @@
+Vue.component('header-links', {
+    template: `
+    <p>Navigate to <a href="https://samir70.github.io/Maths-How2/">Contents</a></p>
+    `
+});

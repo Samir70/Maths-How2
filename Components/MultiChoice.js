eb465669a3c8437784995774f3cb8f6519d3e619
Samir70 <sbetmouni@talktalk.net>
2020-02-22 10:20:44 +0000

    multi-choice component has nextQ enabled

--- Components/MultiChoice.js
+++ Components/MultiChoice.js
@@ -1,12 +1,61 @@
+/*
+This component will show multiple choice questions.
+It requires 
+-- an array of questions
+-- the index of the first questions
+-- a boolean stating whether or not to show a picture (the default is false)
+Each question is an object requiring the following properties
+-- qText : a string with the text of a question
+-- answer : a string which represents the answer
+-- options : an array of strings representing the multiple choice options, 
+    each option will be displayed as a button.
+    Obviously, one of these options needs to match exactly the answer 
+    (this is not checked, it makes it easier 
+        to have many questions whose answers are the same options)
+-- pic : needed if the show-pic attribute is set to true
+-- alt4Pic : alt text for picture, as above 
+*/
+
 Vue.component('multi-choice', {
-    props: ['qData'],
+    props: ['qList', 'firstq', 'showPic'],
+    data: function () {
+        return {
+            showAnswer: false,
+            answerMessage: 'Default answer. Should not be shown',
+            chosenQ: this.qList[this.firstq],
+            unAsked: this.qList.map((x, i)=>i).filter(x=>x!==this.firstq)
+        }
+    },
+    methods: {
+        checkAnswer: function (userAns, qAns) {
+            this.answerMessage = userAns === qAns ?
+                "Correct! That is the " + qAns :
+                "Wrong! That is the " + qAns;
+            this.showAnswer = true;
+        },
+        nextQ: function () {
+            if (this.unAsked.length === 0) {
+                this.unAsked = this.qList.map((x, i) => i);
+            }
+            var qNum = this.unAsked[Math.floor(Math.random() * this.unAsked.length)];
+            this.unAsked = this.unAsked.filter(x => x !== qNum);
+            this.chosenQ = this.qList[qNum];
+            this.showAnswer = false;
+        }
+    },
     template: `
         <div>
-            <h2>{{ qData.qText }}</h2>
-            <button 
-              v-for="b in qData.options" 
-              v-on:click="$emit('answer-given', b, qData.answer)">{{b}}</button>
-            <br>
+            <div v-if="!showAnswer" >
+                <h2>{{ chosenQ.qText }}</h2>
+                <button 
+                  v-for="b in chosenQ.options" 
+                  v-on:click="checkAnswer(b, chosenQ.answer)">{{b}}</button>
+            </div>
+            <div v-if="showAnswer">
+              <h2>{{answerMessage}}</h2>
+              <button v-on:click="nextQ">Next question</button>
+            </div>
+            <img v-if="showPic" v-bind:src="chosenQ.pic" v-bind:alt="chosenQ.alt4Pic" >
         </div>
     `
 })
--- docs/Geometry/Trigonometry/triangleQs.js
+++ docs/Geometry/Trigonometry/triangleQs.js
@@ -1,13 +1,20 @@
 const [ADJ, OPP, HYP] = ['adjacent', 'opposite', 'hypotenuse'];
 
-const triangles2Lable = [
-    { triPic: "triangles/AngleRAdj.png", answer: ADJ },
-    { triPic: "triangles/AngleROpp.png", answer: OPP },
-    { triPic: "triangles/AngleRHyp.png", answer: HYP },
-    { triPic: "triangles/AngleTopAdj.png", answer: ADJ },
-    { triPic: "triangles/AngleTopOpp.png", answer: OPP },
-    { triPic: "triangles/AngleTopHyp.png", answer: HYP },
-    { triPic: "triangles/Adj1.png", answer: ADJ },
-    { triPic: "triangles/Opp1.png", answer: OPP },
-    { triPic: "triangles/NoAngleHyp.png", answer: HYP }
-];
+const lableAOHqs = [
+    { pic: "../docs/Geometry/Trigonometry/triangles/AngleRAdj.png", answer: ADJ },
+    { pic: "../docs/Geometry/Trigonometry/triangles/AngleROpp.png", answer: OPP },
+    { pic: "../docs/Geometry/Trigonometry/triangles/AngleRHyp.png", answer: HYP },
+    { pic: "../docs/Geometry/Trigonometry/triangles/AngleTopAdj.png", answer: ADJ },
+    { pic: "../docs/Geometry/Trigonometry/triangles/AngleTopOpp.png", answer: OPP },
+    { pic: "../docs/Geometry/Trigonometry/triangles/AngleTopHyp.png", answer: HYP },
+    { pic: "../docs/Geometry/Trigonometry/triangles/Adj1.png", answer: ADJ },
+    { pic: "../docs/Geometry/Trigonometry/triangles/Opp1.png", answer: OPP },
+    { pic: "../docs/Geometry/Trigonometry/triangles/NoAngleHyp.png", answer: HYP }
+]
+
+lableAOHqs.forEach(q=>{
+    q.qText = 'At which side does the arrow point?';
+    q.alt4Pic = "Triangle with arrow pointing at a side";
+    q.options = [ADJ, OPP, HYP]
+});
+

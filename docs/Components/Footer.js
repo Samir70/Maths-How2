ff0c9ebc7c748007a8d64df1304e2c14ad264179
Samir70 <sbetmouni@talktalk.net>
2020-02-29 13:00:40 +0000

    include buy me a coffee link in footer

--- docs/Components/Footer.js
+++ docs/Components/Footer.js
@@ -1,9 +1,14 @@
 Vue.component('footer-links', {
-    template: `
-    <div>
+  template: `
+    <div style="text-align:center;">
       <p>Navigate to <a href="https://samir70.github.io/Maths-How2/">Maths-How2 Contents page</a>
-      <br> Copyright Samir70. <a href="https://github.com/Samir70/Maths-How2">Github</a>
-      <br> I make these pages in my spare time. If you like them, you can buy me a coffee (once the link is up and running)</p>
+      <br> Copyright Samir70. Source files available at: <a href="https://github.com/Samir70/Maths-How2">Github</a>
+      <br> I make these pages in my spare time. If you like them, you can 
+      <br> 
+      <a href="https://www.buymeacoffee.com/Samir70">
+        <img src="https://cdn.buymeacoffee.com/buttons/bmc-new-btn-logo.svg" alt="Buy me a coffee">
+        buy me a coffee</a>
+      <br> All donations appreciated and thanks in advance.</p>
     <div>
     `
 });
